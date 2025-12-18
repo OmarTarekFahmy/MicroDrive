b6d5501cb4dc505410e91dc2de3f3b48248682e4
Yassin Hafez <yassinmohamed007@gmail.com>
2025-12-18 09:27:26 +0200

    ...

--- src/main_wifi_camera_simple.c
+++ src/main_wifi_camera_simple.c
@@ -20,9 +20,9 @@
 #define FRAME_HEIGHT 240
 
 // WiFi credentials
-#define WIFI_SSID "youssef's Galaxy S21 Ultra 5G"
-#define WIFI_PASSWORD "Ctiger@YM123"
-#define SERVER_IP "10.79.87.200"
+#define WIFI_SSID "Yassin"
+#define WIFI_PASSWORD "YassoSchool1234"
+#define SERVER_IP "172.20.10.6"
 #define SERVER_PORT 8888
 
 // Frame buffer - same as USB camera
